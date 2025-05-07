5eb4a083401107792b2d2a88f5236b8a05b6050e
carlodev <carlobrunellibs@gmail.com>
2025-05-07 16:48:06 +0200

    second commit

---
+++ examples/EX1.jl
@@ -0,0 +1,38 @@
+using Revise
+using AirfoilCST
+using Plots, CSV, DataFrames
+using Plotly
+
+fname = "DU89.csv" #"n0012.csv"
+NW0 = 20 #number of w0 to parametrize top and bottom
+N1 = 0.5
+N2 = 1.0
+
+
+w0 = w0=0.1.*[ones(NW0); -ones(NW0)]
+x0u, x0l, y0u, y0l = get_airfoil_coordinates(joinpath(@__DIR__, fname))
+
+
+
+
+wu,wl = get_CST_weights(x0u,x0l,y0u,y0l;w0=w0, maxiters=1000,maxtime=20, N1=N1,N2=N2)
+dz = y0u[1] - y0l[end]
+
+(xu,xl), (yu,yl) = CST_airfoil(wu,wl, dz,x0u,x0l; N1=N1,N2=N2)
+
+
+
+
+Plots.default(linewidth=2)
+plotly()
+plot(x0u, y0u, label="original", linecolor=:black, aspect_ratio = 1)
+plot!(x0l, y0l, label=false, linecolor=:black)
+
+plot!(xu, yu, label="CST approximation",linecolor=:red)
+plot!(xl, yl, label=false, linecolor=:red)
+
+maximum(abs.(yu - y0u))
+maximum(abs.(yl - y0l))
+
+plot(abs.(yu - y0u))
+plot!(abs.(yl - y0l))
--- src/AirfoilCST.jl
+++ src/AirfoilCST.jl
@@ -1,5 +1,16 @@
 module AirfoilCST
 
-# Write your package code here.
+#Inspired by the paper "Universal Parametric Geometry Representation Method", Brenda M. Kulfan, 10.2514/1.29958
+
+using LinearAlgebra
+using Optimization, OptimizationBBO
+using XLSX, CSV, DataFrames,FileIO
+
+export get_airfoil_coordinates
+export get_CST_weights
+export CST_airfoil
+
+include("ClassShapes.jl")
+include("ComputeCST.jl")
 
 end
